9072349ffc93c04b861bfbd57719ca6c5ba97502
Adrian Falleiro <adrian.falleiro@analogfolk.com>
2018-05-14 12:51:54 +1000

    Add bootstrap and bridge

---
+++ src/Bootstraps/Slim.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace adrianfalleiro\PHPPM\Slim\Bootstraps;
+
+use Interop\Http\Server\RequestHandlerInterface;
+use PHPPM\Bootstraps\ApplicationEnvironmentAwareInterface;
+use PHPPM\Bootstraps\BootstrapInterface;
+use PHPPM\Bootstraps\HooksInterface;
+use PHPPM\Bootstraps\RequestClassProviderInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Container;
+use Slim\Http\Response;
+use Slim\Http\Request;
+use Slim\Http\Uri;
+use Slim\Http\Headers;
+use Slim\Http\Environment;
+
+class Slim implements ApplicationEnvironmentAwareInterface
+{
+    protected $appenv;
+    protected $debug = false;
+
+    /**
+     * @var App
+     */
+    protected $app;
+
+    public function initialize($appenv, $debug)
+    {
+        $this->appenv = $appenv;
+        $this->debug = $debug;
+
+        $this->app = $this->createSlimApp();
+    }
+
+    protected function createSlimApp()
+    {
+        require 'vendor/autoload.php';
+        $settings = require './src/settings.php';
+
+        $app = new App($settings);
+
+        // Set up dependencies
+        require './src/dependencies.php';
+
+        // Register middleware
+        require './src/middleware.php';
+
+        // Register routes
+        require './src/routes.php';
+
+        return $app;
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Reset Pimple container bindings for certain
+     * default services such as `request` and `environment`
+     * so each request can operate cleanly
+     * 
+     * @param Slim\Http\Request $slimRequest
+     * 
+     * @return {void}
+     */
+    public function resetDefaultServices(Request $slimRequest)
+    {
+        $container = $this->app->getContainer();
+
+        unset($container['request']);
+        unset($container['environment']);
+
+        // Reset app request instance
+        $container['request'] = function($c) use ($slimRequest) {
+            return $slimRequest;
+        };
+
+        // Reset app environment instance
+        $container['environment'] = function($c) use ($slimRequest) {
+            return new Environment($slimRequest->getServerParams());
+        };
+
+        // Reset cached route args
+        $routes = $container['router']->getRoutes();
+        foreach($routes as $route) {
+            $route->setArguments([]);
+        } 
+    }
+}
--- src/Bridges/Slim.php
+++ src/Bridges/Slim.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace adrianfalleiro\PHPPM\Slim\Bootstrap;
+namespace adrianfalleiro\PHPPM\Slim\Bridges;
 
 use Interop\Http\Server\RequestHandlerInterface;
 use PHPPM\Bootstraps\ApplicationEnvironmentAwareInterface;
@@ -8,48 +8,39 @@ use PHPPM\Bootstraps\BootstrapInterface;
 use PHPPM\Bootstraps\HooksInterface;
 use PHPPM\Bootstraps\RequestClassProviderInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Slim\App;
-use Slim\Container;
+use PHPPM\Bridges\BridgeInterface;
 use Slim\Http\Response;
 use Slim\Http\Request;
 use Slim\Http\Uri;
 use Slim\Http\Headers;
 use Slim\Http\Environment;
+use adrianfalleiro\PHPPM\Slim\Bootstraps\Slim as SlimBootstrap;
 
-class Slim implements ApplicationEnvironmentAwareInterface, RequestHandlerInterface
+class Slim implements BridgeInterface
 {
-    protected $appenv;
-    protected $debug = false;
+    /**
+     * An application implementing the HttpKernelInterface
+     *
+     * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    protected $application;
 
     /**
-     * @var App
+     * @var BootstrapInterface
      */
-    protected $app;
+    protected $bootstrap;
 
-    public function initialize($appenv, $debug)
-    {
-
-    }
+    /**
+     * @var string[]
+     */
+    protected $tempFiles = [];
 
     public function bootstrap($appBootstrap, $appenv, $debug)
     {
-        $this->appenv = $appenv;
-        $this->debug = $debug;
-
-  
-        $settings = require __DIR__ . '/../settings.php';
-
-
-        $app = new App($settings);
-        // require __DIR__ . '/../vendor/autoload.php';
-        include __DIR__ . '/../dependencies.php';
-        include __DIR__ . '/../routes.php';
-  
-
         
-        // include '../middlewares.php';
-
-        $this->app = $app;
+        $this->bootstrap = new SlimBootstrap();
+        $this->bootstrap->initialize($appenv, $debug);
+        $this->app = $this->bootstrap->getApp();
     }
 
     /**
@@ -79,41 +70,8 @@ class Slim implements ApplicationEnvironmentAwareInterface, RequestHandlerInterf
     {
         $slimRequest = $this->mapRequest($psrRequest);
 
-        $this->resetDefaultServices($slimRequest);
+        $this->bootstrap->resetDefaultServices($slimRequest);
 
         return $this->app->process($slimRequest, new Response());
-    }
-
-    /**
-     * Reset Pimple container bindings for certain
-     * default services such as `request` and `environment`
-     * so each request can operate cleanly
-     * 
-     * @param Slim\Http\Request $slimRequest
-     * 
-     * @return {void}
-     */
-    protected function resetDefaultServices(Request $slimRequest)
-    {
-        $container = $this->app->getContainer();
-
-        unset($container['request']);
-        unset($container['environment']);
-
-        // Reset app request instance
-        $container['request'] = function($c) use ($slimRequest) {
-            return $slimRequest;
-        };
-
-        // Reset app environment instance
-        $container['environment'] = function($c) use ($slimRequest) {
-            return new Environment($slimRequest->getServerParams());
-        };
-
-        // Reset cached route args
-        $routes = $container['router']->getRoutes();
-        foreach($routes as $route) {
-            $route->setArguments([]);
-        } 
     }
 }
